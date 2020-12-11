1659b7f930f5bf664737b15eda99f36910df87ef
Цилюрик Максим Андреевич <tsilyurik.ma@reliab.tech>
2020-12-12 01:23:11 +0300

    feat: add tempAdd

--- src/main/java/avl/impl/AvlImpl.java
+++ src/main/java/avl/impl/AvlImpl.java
@@ -3,6 +3,7 @@ package avl.impl;
 import avl.Avl;
 
 public class AvlImpl implements Avl {
+    public Node root;
 
     public int height(Node x, Node y) {
         if (x == null && y == null) return 0;
@@ -16,5 +17,24 @@ public class AvlImpl implements Avl {
         else if (x == null) return -y.h;
         else if (y == null) return x.h;
         else return x.h - y.h;
+    }
+
+    public Node tempAdd(Node node, Integer key, Integer value, Node father) {
+        if (node == null) {
+            return new Node(key, value, father);
+        }
+        int compareResult = key.compareTo(node.key);
+        if (compareResult > 0) {
+            node.right = tempAdd(node.right, key, value, node);
+            node.h = height(node.left, node.right) + 1;
+        } else {
+            if (compareResult < 0) {
+                node.left = tempAdd(node.left, key, value, node);
+                node.h = height(node.left, node.right) + 1;
+            } else {
+                node.value = value;
+            }
+        }
+        return node;
     }
 }
