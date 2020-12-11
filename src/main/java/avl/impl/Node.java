7d168e68fc9581f97aa3d7ea3adcc246a4967e72
Цилюрик Максим Андреевич <tsilyurik.ma@reliab.tech>
2020-12-12 00:24:36 +0300

    feat: added Node

---
+++ src/main/java/avl/impl/Node.java
@@ -0,0 +1,19 @@
+package avl.impl;
+
+public class Node {
+    public int h;
+    public int balance;
+    public int key;
+    public int value;
+    public Node left, right, father;
+
+    public Node(int key, int value, Node father) {
+        this.key = key;
+        this.value = value;
+        this.left = this.right = null;
+        this.father = father;
+        this.h = 1;
+        this.balance = 0;
+    }
+
+}
