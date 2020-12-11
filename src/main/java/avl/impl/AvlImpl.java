235db65ac816d5328b2cae068c4b0ad1ceee31b0
Цилюрик Максим Андреевич <tsilyurik.ma@reliab.tech>
2020-12-12 01:32:50 +0300

    feat: add right and left rotation

--- src/main/java/avl/impl/AvlImpl.java
+++ src/main/java/avl/impl/AvlImpl.java
@@ -37,4 +37,56 @@ public class AvlImpl implements Avl {
         }
         return node;
     }
+
+    public Node leftRotation(Node node) {
+        if (node.right.right == null && node.right.left != null) {
+            node.right = rightRotation(node.right);
+            node = leftRotation(node);
+        } else {
+            if (node.right.left == null || node.right.left.h <= node.right.right.h) {
+                Node newNode = node.right;
+                newNode.father = node.father;
+                node.right = newNode.left;
+                if (node.right != null)
+                    node.right.father = node;
+                node.h = height(node.left, node.right) + 1;
+                node.father = newNode;
+                node.balance = balance(node.left, node.right);
+                newNode.left = node;
+                node = newNode;
+                node.balance = balance(node.left, node.right);
+                node.h = height(node.left, node.right) + 1;
+            } else {
+                node.right = rightRotation(node.right);
+                node = leftRotation(node);
+            }
+        }
+        return node;
+    }
+
+    public Node rightRotation(Node node) {
+        if (node.left.right != null && node.left.left == null) {
+            node.left = leftRotation(node.left);
+            node = rightRotation(node);
+        } else {
+            if (node.left.right == null || node.left.right.h <= node.left.left.h) {
+                Node newNode = node.left;
+                newNode.father = node.father;
+                node.left = newNode.right;
+                if (node.left != null)
+                    node.left.father = node;
+                node.h = height(node.left, node.right) + 1;
+                node.father = newNode;
+                node.balance = balance(node.left, node.right);
+                newNode.right = node;
+                node = newNode;
+                node.balance = balance(node.left, node.right);
+                node.h = height(node.left, node.right) + 1;
+            } else {
+                node.left = leftRotation(node.left);
+                node = rightRotation(node);
+            }
+        }
+        return node;
+    }
 }
