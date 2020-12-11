2d2ddd88f5e29f9fb5cd73fa44a802b0a321dc6d
Цилюрик Максим Андреевич <tsilyurik.ma@reliab.tech>
2020-12-12 00:57:36 +0300

    feat: add calculate height

--- src/main/java/avl/impl/AvlImpl.java
+++ src/main/java/avl/impl/AvlImpl.java
@@ -3,4 +3,11 @@ package avl.impl;
 import avl.Avl;
 
 public class AvlImpl implements Avl {
+
+    public int height(Node x, Node y) {
+        if (x == null && y == null) return 0;
+        else if (x == null) return y.h;
+        else if (y == null) return x.h;
+        else return Math.max(x.h, y.h);
+    }
 }
